c5a0e3a0818db2ff361db439fe8c5865245e28cd
Aart Stuurman <aartstuurman@hotmail.com>
2018-10-04 00:47:55 +0200

    MultiArrayData

--- include/ArrayUtil/CArray.hpp
+++ include/ArrayUtil/CArray.hpp
@@ -8,15 +8,34 @@
 
 namespace ArrayUtil {
 
-template <typename Element, size_t... array_sizes>
-class CArray
-{
-    // TODO
-};
+template <typename Element, size_t... dimension_sizes>
+class CArray;
 
 template <typename Element, size_t array_size>
 class CArray<Element, array_size>
 {
+    // TODO
+    
+public:
+};
+
+// workaround because some compilers don't understand fold expressions in template parameters
+namespace CArrayInternal {
+    template <size_t... values>
+    struct TemplateSum
+    {
+        static constexpr size_t sum = (values*...);
+    };
+}
+// Array that is the right size to hold the given multidimensional array
+template <typename Element, size_t... dimension_sizes>
+using MultiArrayData = CArray<Element, CArrayInternal::TemplateSum<dimension_sizes...>::sum>;
+
+template <typename Element, size_t... dimension_sizes>
+class CArray
+{
+    MultiArrayData<Element, dimension_sizes...> data;
+public:
     // TODO
 };
 
---
+++ test/CArray.cpp
@@ -0,0 +1,6 @@
+#include "ArrayUtil/CArray.hpp"
+
+int main()
+{
+    return 0;
+}
